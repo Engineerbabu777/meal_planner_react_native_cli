a6b513efde385ae44fc33a59702e8fd26178b97b
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-06 21:42:05 +0500

    Navigation added

---
+++ screens/MenuScreen.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const MenuScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>MenuScreen</Text>
+    </View>
+  );
+};
+
+export default MenuScreen;
+
+const styles = StyleSheet.create({});
