14743f966072a09516470c37b001d78f8e1cf94d
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-06 17:43:01 +0500

    mongodb added

--- api/index.js
+++ api/index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
 const cors = require('cors');
 app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-
 app.listen(port, () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch(error => {
+      console.log('Error connecting to MongoDb', error);
+    });
+
   console.log('Server is running on port 3000');
 });
-
